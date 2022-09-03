58dde3a70d628ac4548afb744a0668dd79c151a8
Eselase-Noble <nevulley@st.ug.edu.gh>
2022-09-03 23:30:02 +0000

    Update

--- G22-DSA-INVENTORYSYSTEM/src/main/java/com/example/g22dsainventorysystem/structures/ProductCategoryManagement.java
+++ G22-DSA-INVENTORYSYSTEM/src/main/java/com/example/g22dsainventorysystem/structures/ProductCategoryManagement.java
@@ -1,4 +1,31 @@
 package com.example.g22dsainventorysystem.structures;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class ProductCategoryManagement {
+
+
+    public  ProductCategoryManagement(){}
+
+    public ObservableStack<Category> getCategories() {
+        Connection connection = TestConnection.ConnectionUtil.connectdb();
+        ObservableStack<Category> stack = new ObservableStack<>();
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT Product*, Category.Category_Name FROM Category INNER JOIN Category ON Product.Category");
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                stack.push(new Category(resultSet.getInt("Category_ID"), resultSet.getString("Category_Name"), resultSet.getString("Description")));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return stack;
+
+
+    }
 }
